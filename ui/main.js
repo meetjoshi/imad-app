13c74f9cd060c74c3de32a8ee23bb974751e6ded
Meet Joshi <meetjoshi@users.noreply.github.com>
2017-08-23 19:19:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,12 @@ element.innerHTML = 'New value';
 
 // Move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginleft = marginLeft + 'px';
+}
 
 img.onclick = function () {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight, 100);
 };

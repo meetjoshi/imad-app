5725e68db7277759465373b7403e524773fd0dae
Meet Joshi <meetjoshi@users.noreply.github.com>
2017-08-24 12:10:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,8 +27,6 @@ button.onclick = function () {
 
 // Submit name
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     
@@ -55,6 +53,8 @@ submit.onclick = function () {
     };
     
     // Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', 'http://meetjoshiiitkgp.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };

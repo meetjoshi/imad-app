37903216894e536b5cc6d61b711e1ea36c4f77c6
Meet Joshi <meetjoshi@users.noreply.github.com>
2017-09-19 18:39:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get('/counter', function (req, res) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
         }
     });
 });

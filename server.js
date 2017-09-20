1dab3fbccb56b296f57cd61eabd37a58fa5fb376
Meet Joshi <meetjoshi@users.noreply.github.com>
2017-09-20 15:50:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,43 +14,6 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-
-var articles = {
-    'article-one': {
-        title: 'Meet Joshi | Article One',
-        heading: 'Article One',
-        date: 'Sep 5, 2016',
-        content: `
-            <p>
-                This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-            </p>
-            <p>
-                This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-            </p>
-            <p>
-                This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-            </p>`
-    },
-    'article-two': {
-        title: 'Meet Joshi | Article Two',
-        heading: 'Article Two',
-        date: 'Sep 10, 2016',
-        content: `
-            <p>
-                This is the content for my second article.
-            </p>`
-    },
-    'article-three': {
-        title: 'Meet Joshi | Article Three',
-        heading: 'Article Three',
-        date: 'Sep 15, 2016',
-        content: `
-            <p>
-                This is the content for my third article.
-            </p>`
-    }
-};
-
 function createTemplate (data) {
     var title = data.title;
     var date = data.date;

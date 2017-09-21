fcc6255bb32dd10870cb918420fc57201f32b2fe
Meet Joshi <meetjoshi@users.noreply.github.com>
2017-09-21 18:51:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post('/create-user', function (req, res) {
             res.status(500).send(err.toString());
         } else {
             
-            res.send('User successfully created: ' + username));
+            res.send('User successfully created: ' + username);
         }
     });
 });

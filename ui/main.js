e9e203d5b7cbac6056d241f7890531398708d039
Meet Joshi <meetjoshi@users.noreply.github.com>
2017-09-21 20:09:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,5 +30,6 @@ submit.onclick = function () {
     console.log(username);
     console.log(password);
     request.open('POST', 'http://meetjoshiiitkgp.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username: username, password: password}));
 };

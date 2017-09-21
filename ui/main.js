b5d84348cb5be15dea9107122e71f72586b22274
Meet Joshi <meetjoshi@users.noreply.github.com>
2017-09-21 19:57:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,6 @@
-// Counter code
-var button = document.getElementById('counter');
-var counter = 0;
 
-button.onclick = function () {
-    
-    // Create a request object
-    var request = new XMLHttpRequest();
-    
-    // Capture the response and store it in a variable
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            // Take some action
-            if (request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-        // Not done yet
-    };
-    
-    // Make the request
-    request.open('GET', 'http://meetjoshiiitkgp.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
 
-// Submit name
+// Submit username/password to login
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     

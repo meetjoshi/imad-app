d2d1cd466f6f3823c3c775ca74a9c1ce4bcd79f5
Meet Joshi <meetjoshi@users.noreply.github.com>
2017-09-21 19:23:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var config = {
     user: 'meetjoshiiitkgp',
     database: 'meetjoshiiitkgp',
     host: 'db.imad.hasura-app.io',
-    port: '5432',
+    port: 5432,
     password: process.env.DB_PASSWORD
 };
 
